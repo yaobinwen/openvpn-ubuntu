abce206143801020fa0220a48065878fd1773d65
Alberto Gonzalez Iniesta <agi@inittab.org>
2006-04-05 12:17:26 +0200

    2.0.6-1 (patches unapplied)
    Imported using git-ubuntu import.

--- openvpn.h
+++ openvpn.h
@@ -117,6 +117,23 @@ struct context_buffers
   struct buffer read_tun_buf;
 };
 
+/* 
+ * level 0 context contains data related to
+ * once-per OpenVPN instantiation events
+ * such as daemonization.
+ */
+struct context_0
+{
+  /* workspace for get_pid_file/write_pid */
+  struct pid_state pid_state;
+
+  /* workspace for --user/--group */
+  bool uid_gid_specified;
+  bool uid_gid_set;
+  struct user_state user_state;
+  struct group_state group_state;
+};
+
 /*
  * Contains the persist-across-restart OpenVPN tunnel instance state.
  * Reset only for SIGHUP restarts.
@@ -335,18 +352,6 @@ struct context_2
    */
   bool ipv4_tun;
 
-  /* workspace for get_pid_file/write_pid */
-  struct pid_state pid_state;
-
-  /* workspace for --user/--group */
-  bool uid_gid_specified;
-  bool uid_gid_set;
-  struct user_state user_state;
-  struct group_state group_state;
-
-  /* temporary variable */
-  bool did_we_daemonize;
-
   /* should we print R|W|r|w to console on packet transfers? */
   bool log_rw;
 
@@ -398,7 +403,7 @@ struct context_2
   in_addr_t push_ifconfig_local;
   in_addr_t push_ifconfig_remote_netmask;
 
-  /* client authentication state */
+  /* client authentication state, CAS_SUCCEEDED must be 0 */
 # define CAS_SUCCEEDED 0
 # define CAS_PENDING   1
 # define CAS_FAILED    2
@@ -446,6 +451,14 @@ struct context
 
   /* signal info */
   struct signal_info *sig;
+
+  /* set to true after we daemonize */
+  bool did_we_daemonize;
+
+  /* level 0 context contains data related to
+     once-per OpenVPN instantiation events
+     such as daemonization */
+  struct context_0 *c0;
 
   /* level 1 context is preserved for
      SIGUSR1 restarts, but initialized
