e5a23527e01875d9a77c127c9b14939e12710711
Thierry Carrez (ttx) <thierry.carrez@ubuntu.com>
2010-10-20 16:22:25 +0200

    close_socket_before_scripts.patch
    No DEP3 Subject or Description header found
    
    Gbp-Pq: close_socket_before_scripts.patch.

--- socket.c
+++ socket.c
@@ -1328,6 +1328,10 @@ link_socket_init_phase1 (struct link_socket *sock,
       resolve_bind_local (sock);
       resolve_remote (sock, 1, NULL, NULL);
     }
+
+  /* set socket file descriptor to not pass across execs, so that
+     scripts don't have access to it */
+  set_cloexec (sock->sd);
 }
 
 /* finalize socket initialization */
@@ -1532,10 +1536,6 @@ link_socket_init_phase2 (struct link_socket *sock,
 
   /* set socket to non-blocking mode */
   set_nonblock (sock->sd);
-
-  /* set socket file descriptor to not pass across execs, so that
-     scripts don't have access to it */
-  set_cloexec (sock->sd);
 
 #ifdef ENABLE_SOCKS
   if (socket_defined (sock->ctrl_sd))
